ea9dadfcc87d9ab4a0838b6d74d6d007ace58e79
Aleksandr Afonin <varsup@yandex.ru>
2024-07-01 11:54:27 +0300

    homework16

--- server/src/main/java/ru/otus/june/chat/server/Server.java
+++ server/src/main/java/ru/otus/june/chat/server/Server.java
@@ -8,18 +8,25 @@ import java.util.List;
 public class Server {
   private int port;
   private List<ClientHandler> clients;
+  private AuthenticationProvider authenticationProvider;
 
   public Server(int port) {
     this.port = port;
     this.clients = new ArrayList<>();
+    this.authenticationProvider = new InMemoryAuthenticationProvider(this);
+  }
+
+  public AuthenticationProvider getAuthenticationProvider() {
+    return authenticationProvider;
   }
 
   public void start() {
     try (ServerSocket serverSocket = new ServerSocket(port)) {
       System.out.println("Сервер запущен на порту: " + port);
+      authenticationProvider.initialize();
       while (true) {
         Socket socket = serverSocket.accept();
-        subscribe(new ClientHandler(this, socket));
+        new ClientHandler(this, socket);
       }
     } catch (Exception e) {
       e.printStackTrace();
@@ -33,6 +40,9 @@ public class Server {
 
   public synchronized void unsubscribe(ClientHandler clientHandler) {
     clients.remove(clientHandler);
+    if (clientHandler.getUsername() == null){
+      return;
+    }
     broadcastMessage("Из чата вышел: " + clientHandler.getUsername());
   }
 
@@ -56,5 +66,14 @@ public class Server {
         break;
       }
     }
+  }
+
+  public synchronized boolean isUsernameBusy(String username){
+    for(ClientHandler c : clients){
+      if(c.getUsername().equals(username)){
+        return true;
+      }
+    }
+    return false;
   }
 }
